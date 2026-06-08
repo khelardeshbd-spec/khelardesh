c98b162971420a424bb88f69a9288ed631395205
Khelardeshbd <khelardeshbd@gmail.com>
2026-06-09 05:04:52 +0600

    l

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['pub-YOUR_R2_ACCOUNT.r2.dev'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.r2.dev',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        stream: false,
+        crypto: false,
+        net: false,
+        tls: false,
+        dns: false,
+        child_process: false,
+      }
+    }
+    return config
   },
 }
 
